63e6e95266909e231b59c296b4a1da9f0534d922
gausss <gausssven@gmail.com>
2017-06-30 21:07:27 +0200

    Hallo Fa

--- app/src/main/java/com/example/fabian/headfirst/Example.java
+++ app/src/main/java/com/example/fabian/headfirst/Example.java
@@ -3,13 +3,12 @@ package com.example.fabian.headfirst;
 /**
  * Created by fabian on 30.06.17.
  */
-
 public class Example {
 
     public static void main (String[] args) {
         System.out.println("I Rule!");
         System.out.println("HAHAH");
         System.out.println("HAHAH");
-
+        System.out.println("Hallo Fa.");
     }
 }
