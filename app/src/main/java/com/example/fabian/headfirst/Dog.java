e4f66d6b1f57a7585406a2ca5b824c27dd2e5af4
fa3000 <fagauss@gmail.com>
2017-07-02 18:21:37 +0200

    Test

---
+++ app/src/main/java/com/example/fabian/headfirst/Dog.java
@@ -0,0 +1,16 @@
+package com.example.fabian.headfirst;
+
+/**
+ * Created by fabian on 02.07.17.
+ */
+
+public class Dog {
+    String name;
+    int age;
+
+    void bark() {
+        System.out.println("wuff wuff");
+    }
+
+
+}
--- app/src/main/java/com/example/fabian/headfirst/Example.java
+++ app/src/main/java/com/example/fabian/headfirst/Example.java
@@ -5,11 +5,25 @@ package com.example.fabian.headfirst;
  */
 public class Example {
     public static void main (String[] args) {
-        System.out.println("I Rule!");
-        System.out.println("HAHAH");
-        System.out.println("HAHAH");
-        System.out.println("Hallo Fa.");
 
+        Dog[] dogs = new Dog[4];
 
+       dogs[1]  = new Dog();
+        dogs[2] = new Dog();
+        dogs[3] = new Dog();
+
+        dogs[1].name="Fabian";
+        dogs[2].name="Dennis";
+        dogs[3].name="Sven";
+        dogs[1].age= 4;
+        dogs[2].age=5;
+        dogs[3].age=3;
+
+        dogs[1].bark();
+        System.out.println("Mein Name ist " +dogs[1].name+ ". Ich bin "+dogs[1].age+" Jahre alt.");
+        dogs[2].bark();
+        System.out.println("Mein Name ist " +dogs[2].name+ ". Ich bin "+dogs[2].age+" Jahre alt.");
+        dogs[3].bark();
+        System.out.println("Mein Name ist " +dogs[3].name+ ". Ich bin "+dogs[3].age+" Jahre alt.");
     }
 }
